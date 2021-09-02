681daeca365ab24295ac786b244ef10beebccbc3
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-01 20:28:55 -0400

    models/index.js removed reference examples

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,22 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-//EX Ship.belongsTo(Captain, { targetKey: 'name', foreignKey: 'captainName' });
-// This creates a foreign key called `captainName` in the source model (Ship)
-// which references the `name` field from the target model (Captain).
 Product.belongsTo(Category, { foreignKey: "category_id"});
 
 // Categories have many Products
-// Bar.hasMany(Baz, { sourceKey: 'title', foreignKey: 'barTitle' });
 Category.hasMany(Product, { foreignKey: "category_id"});
 
 // Products belongToMany Tags (through ProductTag)
-// Foo.belongsToMany(Bar, { through: 'foo_bar' });
-// This creates a junction table `foo_bar` with fields `fooId` and `barId`
 Product.belongsToMany(Tag, { through: { model: ProductTag }});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: { model: ProductTag }});
+Tag.belongsToMany(Product, { through: { model: Product }});
 
 module.exports = {
   Product,

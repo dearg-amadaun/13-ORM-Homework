c556077cb0b488d9f79c2cc65f30fd002d9a3bdd
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-01 20:06:32 -0400

    models_producttags.js finished

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
@@ -18,3 +37,4 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+ProductTag

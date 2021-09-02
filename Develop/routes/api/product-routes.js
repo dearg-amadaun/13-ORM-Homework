5d0a9343d6eee1ef74653ac6cae547c671244a60
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-01 23:54:06 -0400

    clean up routes files

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -14,10 +14,21 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 // get one product
 // find a single product by its `id`
 // be sure to include its associated Category and Tag data
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, { include: Category, Tag });
+    if (!productData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product

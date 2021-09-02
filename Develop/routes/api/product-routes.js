e81721b599f202057c4b9784aaec160e155f1411
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-02 14:23:27 -0400

    added product delete route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -105,8 +105,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+    // delete one product by its `id` value
+    try {
+      const productData = await product.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!productData) {
+        res.status(404).json('No Product found with this id!');
+      }
+      res.status(200).json(productData);
+    } catch {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
